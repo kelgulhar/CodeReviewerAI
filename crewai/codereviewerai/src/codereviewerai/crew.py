59e05c2f90f2d004190d55b9906afd9cca3bf438
kelgulhar <stefan.zuza@yahoo.de>
2026-03-08 15:47:17 +0100

    feat: enable first run of crew ai

--- crewai/codereviewerai/src/codereviewerai/crew.py
+++ crewai/codereviewerai/src/codereviewerai/crew.py
@@ -1,7 +1,10 @@
-from crewai import Agent, Crew, Process, Task
+from crewai import Agent, Crew, Process, Task, LLM
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
+
+import os
+
 # If you want to run a snippet of code before or after the crew starts,
 # you can use the @before_kickoff and @after_kickoff decorators
 # https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
@@ -12,6 +15,15 @@ class Codereviewerai():
 
     agents: List[BaseAgent]
     tasks: List[Task]
+    
+    # this is onlny optional for those who have no acccess to public LLM with tool calling support
+    #REMARK: there is a bug with python 3.12 and litellm causing logging errors during shutdown that are not relevant - you can ignore
+    local_llm = LLM(
+        model=os.getenv("MODEL"),
+        base_url="https://api.openai.com/v1",
+        #base_url="https://daystrom.ditm.at:4000/v1",
+        api_key=os.getenv("OPENAI_API_KEY"),
+    )
 
     # Learn more about YAML configuration files here:
     # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
@@ -20,34 +32,35 @@ class Codereviewerai():
     # If you would like to add tools to your agents, you can learn more about it here:
     # https://docs.crewai.com/concepts/agents#agent-tools
     @agent
-    def researcher(self) -> Agent:
+    def static_analyst(self) -> Agent:
         return Agent(
-            config=self.agents_config['researcher'], # type: ignore[index]
+            config=self.agents_config['static_analyst'], # type: ignore[index]
+            llm=self.local_llm,
             verbose=True
         )
 
-    @agent
-    def reporting_analyst(self) -> Agent:
-        return Agent(
-            config=self.agents_config['reporting_analyst'], # type: ignore[index]
-            verbose=True
-        )
+    # @agent
+    # def reporting_analyst(self) -> Agent:
+    #     return Agent(
+    #         config=self.agents_config['reporting_analyst'], # type: ignore[index]
+    #         verbose=True
+    #     )
 
     # To learn more about structured task outputs,
     # task dependencies, and task callbacks, check out the documentation:
     # https://docs.crewai.com/concepts/tasks#overview-of-a-task
     @task
-    def research_task(self) -> Task:
+    def static_analysis_task(self) -> Task:
         return Task(
-            config=self.tasks_config['research_task'], # type: ignore[index]
+            config=self.tasks_config['static_analysis_task'], # type: ignore[index]
         )
 
-    @task
-    def reporting_task(self) -> Task:
-        return Task(
-            config=self.tasks_config['reporting_task'], # type: ignore[index]
-            output_file='report.md'
-        )
+    # @task
+    # def reporting_task(self) -> Task:
+    #     return Task(
+    #         config=self.tasks_config['reporting_task'], # type: ignore[index]
+    #         output_file='report.md'
+    #     )
 
     @crew
     def crew(self) -> Crew:
